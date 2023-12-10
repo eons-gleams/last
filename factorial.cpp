e1d31b17bd3cd66cbc3ddf6dbe319a00fe19c993
David Sutton <153441616+eons-gleams@users.noreply.github.com>
2023-12-10 22:07:33 +0700

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,10 @@
+// factorial.cpp
+#include "factorial.h"
+
+long long calculateFactorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * calculateFactorial(n - 1);
+    }
+}
